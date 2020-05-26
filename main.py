19283d089c8869092037d60a2706fbdd31c93376
ryaalbr <63967422+ryaalbr@users.noreply.github.com>
2020-05-26 09:24:12 -0700

    Update main.py

--- main.py
+++ main.py
@@ -111,12 +111,12 @@ while True:
         pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
 
     # Paddle and ball collisions
-    if (ball.xcor() > 340  and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
+    if (ball.xcor() > 340  and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 60 and ball.ycor() > paddle_b.ycor() - 60):
         ball.setx(340)
         ball.dx *= -1
         os.system("afplay bounce.wav&")
 
-    if (ball.xcor() < -340  and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40):
+    if (ball.xcor() < -340  and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 60 and ball.ycor() > paddle_a.ycor() - 60):
         ball.setx(-340)
         ball.dx *= -1
         os.system("afplay bounce.wav&")
